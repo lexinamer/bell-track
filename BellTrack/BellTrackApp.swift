b63042fd88a613920cca1a1fb2e2caf11aad69c5
Lexi Namer <lexinamer@gmail.com>
2025-12-29 16:21:13 -0600

    Initial Commit

---
+++ BellTrack/BellTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BellTrackApp.swift
+//  BellTrack
+//
+//  Created by Lexi Namer on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct BellTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
